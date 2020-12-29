9bd347e9b95c32ea94deee78b209f3193050258c
Imperial Design <amsanchez@knox.edu>
2020-12-28 21:21:23 -0600

    add welcome cli

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-const connection = require("./db/database");
-const inquirer = require(inquirer);
+const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user:"root",
+  password: "KnoxFox#25",
+  database: "tracker_db"
+})
+
+const welcome = function() {
+  inquirer
+  .prompt({
+      type: "list",
+      name: "welcome",
+      message: "Welcome to the Employee Tracker Database! Where would you like to start?",
+      choices: [
+        "Employees",
+        "Roles",
+        "Departments"
+      ]
+    })
+
+  .then(function(answer) {
+    console.log(answer);
+    switch (answer.welcome) {
+      case "Employees":
+      viewEmployees ();
+      break;
+      case "Roles":
+      viewRoles();
+      break;
+      case "Departments":
+        viewDepartments();
+        break;
+    }
+    });
+  };
+  welcome();
+
+  
