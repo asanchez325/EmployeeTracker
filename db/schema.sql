42224dcb2120941ded7f68067f5e90996dad2d78
Imperial Design <amsanchez@knox.edu>
2020-12-29 14:28:25 -0600

    remove imaginary spaces from schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,33 +2,37 @@ DROP DATABASE IF EXISTS tracker_db;
 CREATE DATABASE tracker_db;
 USE tracker_db;
 
+
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
-   PRIMARY KEY (id),
-   );
-   
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
+);
+
 CREATE TABLE roles(
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+-- REFERENCES means that the Foreign key corresponds to the primary key of the other table
+-- ON DELETE CASCADE means that if this key is deleted, it will delete values on all the other tables with this key
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  roles_id INT,
-  manager_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+
 );
 
