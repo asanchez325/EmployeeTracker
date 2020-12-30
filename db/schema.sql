bbdc2ec2597854a348efbb40b30702d55c03ac6c
Imperial Design <amsanchez@knox.edu>
2020-12-30 03:58:07 -0600

    Reordered drop tables to fix reference error

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ USE tracker_db;
 
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
